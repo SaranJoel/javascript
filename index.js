97be8880858ae88491978cfe2fc5535d2f11b7e0
SaranJoel <joel.david3721@gmail.com>
2024-06-24 19:52:27 -0400

    added counter like program where pressing btn increse adn decrease the number

--- index.js
+++ index.js
@@ -23,13 +23,33 @@
 //     document.getElementById('h1').textContent = `HI this is ${inputText} and you are in H1`
 // }
 
-const PI  = 3.147
-let radius;
-let curcumference
+// const PI  = 3.147
+// let radius;
+// let curcumference
 
-document.getElementById('myButton').onclick = function(){
-    radius = document.getElementById('myText').value;
-    radius = Number(radius);
-    curcumference = 2* PI *radius;
-    document.getElementById('h3').textContent = `the curcumference of the circle with radius ${radius} is ${curcumference}`
+// document.getElementById('myButton').onclick = function(){
+//     radius = document.getElementById('myText').value;
+//     radius = Number(radius);
+//     curcumference = 2* PI *radius;
+//     document.getElementById('h3').textContent = `the curcumference of the circle with radius ${radius} is ${curcumference}`
+// }
+const decreaseBtn = document.getElementById('decrease');
+const resetBtn = document.getElementById('reset');
+const increaseBtn = document.getElementById('increase');
+const count = document.getElementById('count');
+let countNum = 0;
+
+increaseBtn.onclick = function(){
+    countNum++;
+     count.textContent = countNum
+}
+
+resetBtn.onclick = function(){
+    countNum = 0
+    count.textContent=countNum
+}
+
+decreaseBtn.onclick = function(){
+    countNum--;
+    count.textContent = countNum;
 }
